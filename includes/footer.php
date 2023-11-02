5d19517e12e94446d17ad8253f84c92962d1a627
Adriano <adrianocamilo055@gmail.com>
2023-11-02 18:19:36 -0300

    versão simplificada

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="rodape">
 	<div class="comun">
 		<ul class="lista-footer">
-			<li class="item-footer"><a href="contato.php" class="ancora">Entre em contato</a></li>
+			<li class="item-footer"><a href="contato.html" class="ancora">Entre em contato</a></li>
 			<li class="item-footer">Termos de uso</li>
 			<li class="item-footer">Politicas de privacidade</li>
 		</ul>
--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,15 @@
 <header id="header">
         <div class="header-logo">
-            <a href="index.php"><img src="./imgs/Logos Coletivo Casulo-08.png" alt="logo-w-d2" id="logo-w-d2"></a>
+            <a href="index.html"><img src="./imgs/Logos Coletivo Casulo-08.png" alt="logo-w-d2" id="logo-w-d2"></a>
         </div>
         <ul class="lista">
-            <li class="item-d"><a href="index.php#d4" class="ancora">Escola</a></li>
-            <li class="item-d"><a href="sobreColetivo.php" class="ancora">Sobre Nós</a></li>
+            <li class="item-d"><a href="index.html#d4" class="ancora">Escola</a></li>
+            <li class="item-d"><a href="sobreColetivo.html" class="ancora">Sobre Nós</a></li>
             <img src="./imgs/Icon-D2.png" alt="icon-d2" id="icon-d2" onclick="handleMenu()">
         </ul>
     </header>
 
     <div id="menu">
-        <li class="item-m"><a href="index.php #d4" class="ancora">Escola</a></li>
-        <li class="item-m"><a href="sobreColetivo.php" class="ancora">Sobre Nós</a></li>
+        <li class="item-m"><a href="index.html#d4" class="ancora">Escola</a></li>
+        <li class="item-m"><a href="sobreColetivo.html" class="ancora">Sobre Nós</a></li>
     </div>
